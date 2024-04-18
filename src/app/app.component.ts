095130b2ae1e06de09f22a8ffc08763e334ea7a6
Asambler2 <chema508@hotmail.com>
2024-04-18 13:15:00 +0200

    Ej terminado

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mdf';
+
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl()
+  })
+
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+

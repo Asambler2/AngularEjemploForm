d5bc133e409e83a465a035722ac4088f1a7b627b
Asambler2 <chema508@hotmail.com>
2024-04-18 13:31:37 +0200

    Terminado del todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,18 @@ export class AppComponent {
   title = 'mdf';
 
   contactForm = new FormGroup({
-    firstname: new FormControl(),
+    /*firstname: new FormControl({ value: 'Rahul', disabled: true }),*/
+    firstname: new FormControl('', [Validators.required, Validators.minLength(10)]),
     lastname: new FormControl(),
     email: new FormControl(),
     gender: new FormControl(),
     isMarried: new FormControl(),
-    country: new FormControl()
+    address: new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode: new FormControl(),
+      country: new FormControl(),
+    })
   })
 
 
